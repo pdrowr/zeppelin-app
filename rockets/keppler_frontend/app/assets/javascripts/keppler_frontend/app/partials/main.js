2aa24d1f7ad7ef5096c1c8e7c85cea9f47cafdea
Gladibeth <gladibethdelos@gmail>
2019-06-13 10:40:56 -0400

    Add preloader

--- rockets/keppler_frontend/app/assets/javascripts/keppler_frontend/app/partials/main.js
+++ rockets/keppler_frontend/app/assets/javascripts/keppler_frontend/app/partials/main.js
@@ -1,3 +1,12 @@
 $('.menu-toggle').click(function () {
   $('aside').toggleClass('active-menu');
 })
+
+$(window).on("load", function (e) {
+  setTimeout(function () {
+    $('#preloader').fadeOut(500, function () {
+      $(this).remove();
+    });
+    $('.body').css('overflow-y', 'auto');
+  }, 1000);
+});
